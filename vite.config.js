ee79b733ac1e303761db7acbdb141bba63c783ea
GabrielZolk <dialixkat@gmail.com>
2023-09-01 20:14:56 -0300

    1.0

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
-    port: 3000,
-  },
-  base: 'http://localhost:3000'
+  // server: {
+  //   port: 3000,
+  // },
+  // base: 'http://localhost:3000'
 })
